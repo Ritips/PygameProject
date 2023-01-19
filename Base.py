304451b1de9090cc56608444b251de68e11899ea
StormVortex <undem1390@gmail.com>
2023-01-19 20:28:35 +0300

    EscMenu. Restart game

--- Base.py
+++ Base.py
@@ -1,8 +1,6 @@
 from StartScreen import start_screen
 from Player import Player
 from Queen import Queen
-from LoadImage import load_image
-from Slime import Slime
 from Constructions import *
 from LoadLevel import *
 from DefinePlayerLevel import *
@@ -14,52 +12,70 @@ pygame.init()
 
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-esc_menu_group = pygame.sprite.Group()
 
 
 class EscMenu(pygame.sprite.Sprite):
     def __init__(self):
-        super(EscMenu, self).__init__(esc_menu_group)
+        super(EscMenu, self).__init__(sprites)
         settings_width = 400 * width // 800
         settings_height = 400 * height // 600
         space_x = 200 * width // 800
         space_y = 100 * height // 600
         self.exit_width = button_width
         self.exit_height = button_height
-        self.exit_space_x, self.exit_space_y = (settings_width - self.exit_width) // 2, settings_height // 2
-        self.btn_settings_space_x = 200 * settings_width // 800
-        self.btn_settings_space_y = 100 * settings_height // 600
-        self.btn_settings_coords = []
-        self.settings = [(800, 600), (1280, 720), (1920, 1080)]
-        try:
-            image = load_image('InterfaceSettings.png')
-            image = pygame.transform.scale(image, (settings_width, settings_height))
-            self.image = image
-        except ValueError:
-            self.image = pygame.Surface((settings_width, settings_height), pygame.SRCALPHA)
-            pygame.draw.rect(self.image, black, (0, 0, settings_width, settings_height))
-            pygame.draw.rect(self.image, dark_grey, (0, 0, settings_width, self.exit_height))
-            pygame.draw.rect(self.image, dark_grey, (0, 0, settings_width, settings_height), 1)
+        self.exit_space_x, self.exit_space_y = (settings_width - self.exit_width) // 2, settings_height // 3
+        self.image = pygame.Surface((settings_width, settings_height), pygame.SRCALPHA)
+        pygame.draw.rect(self.image, black, (0, 0, settings_width, settings_height))
+        pygame.draw.rect(self.image, dark_grey, (0, 0, settings_width, self.exit_height))
+        pygame.draw.rect(self.image, dark_grey, (0, 0, settings_width, settings_height), 1)
         self.rect = pygame.Rect((space_x, space_y, settings_width, settings_height))
         font_size_menu = 40 * width // 800
         self.font = pygame.font.Font(None, font_size_menu)
         self.btn_exit()
+        self.btn_continue()
+        self.btn_return_start_menu()
 
     def btn_exit(self):
-        try:
-            image = load_image('InterfaceSettingsButtonExit.png')
-            image = pygame.transform.scale(image, (self.exit_width, self.exit_height))
-        except ValueError:
-            image = pygame.Surface((self.exit_width, self.exit_height), pygame.SRCALPHA)
-            pygame.draw.rect(image, dark_grey, (0, 0, self.exit_width, self.exit_height))
-            text = self.font.render('  Close Game', True, white)
-            image.blit(text, (0, (8 * width // 800)))
+        image = pygame.Surface((self.exit_width, self.exit_height), pygame.SRCALPHA)
+        pygame.draw.rect(image, dark_grey, (0, 0, self.exit_width, self.exit_height))
+        text = self.font.render('  Close Game', True, white)
+        image.blit(text, (0, (8 * width // 800)))
         self.image.blit(image, (self.exit_space_x, self.exit_space_y))
+
+    def btn_continue(self):
+        image = pygame.Surface((self.exit_width, self.exit_height), pygame.SRCALPHA)
+        pygame.draw.rect(image, dark_grey, (0, 0, self.exit_width, self.exit_height))
+        text = self.font.render('    Continue', True, white)
+        image.blit(text, (0, (8 * width // 800)))
+        self.image.blit(image, (self.exit_space_x, self.exit_space_y + button_height + 15 * height // 600))
 
     def close(self, pos):
         range_x = self.rect.x + self.exit_space_x
         if pos[0] in range(range_x, range_x + self.exit_width + 1):
             range_y = self.rect.y + self.exit_space_y
+            if pos[1] in range(range_y, range_y + self.exit_height + 1):
+                return True
+        return False
+
+    def continue_game(self, pos):
+        range_x = self.rect.x + self.exit_space_x
+        if pos[0] in range(range_x, range_x + self.exit_width + 1):
+            range_y = self.rect.y + self.exit_space_y + button_height + 15 * height // 600
+            if pos[1] in range(range_y, range_y + self.exit_height + 1):
+                return True
+        return False
+
+    def btn_return_start_menu(self):
+        image = pygame.Surface((self.exit_width, self.exit_height), pygame.SRCALPHA)
+        pygame.draw.rect(image, dark_grey, (0, 0, self.exit_width, self.exit_height))
+        text = self.font.render('    Start menu', True, white)
+        image.blit(text, (0, (8 * width // 800)))
+        self.image.blit(image, (self.exit_space_x, self.exit_space_y + button_height * 2 + 30 * height // 600))
+
+    def return_start_menu(self, pos):
+        range_x = self.rect.x + self.exit_space_x
+        if pos[0] in range(range_x, range_x + self.exit_width + 1):
+            range_y = self.rect.y + self.exit_space_y + button_height * 2 + 30 * height // 600
             if pos[1] in range(range_y, range_y + self.exit_height + 1):
                 return True
         return False
@@ -89,19 +105,32 @@ def start_game():
     color, player = draw_level(index=index, level_draw=level_to_draw)
     change_image_time = 0
     esc_menu = None
-    while True:
+    running = True
+    while running:
         screen.fill(color)
         if esc_menu:
+            pygame.mouse.set_visible(True)
+            sprites.draw(screen)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     exit()
-                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and esc_menu.close(event.pos):
-                    exit()
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    if esc_menu.close(event.pos):
+                        exit()
+                    elif esc_menu.continue_game(event.pos):
+                        esc_menu.kill()
+                        esc_menu = None
+                    elif esc_menu.return_start_menu(event.pos):
+                        sprites.empty()
+                        enemies.empty()
+                        group_player.empty()
+                        return True
                 if event.type == pygame.KEYDOWN and event.key == 27:
                     esc_menu.kill()
                     esc_menu = None
-            esc_menu_group.draw(screen)
+            pygame.display.flip()
             continue
+        pygame.mouse.set_visible(False)
         change_image_time += 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -110,15 +139,21 @@ def start_game():
                 player.func_attack(True)
             if event.type == pygame.KEYDOWN and event.key == 27:
                 esc_menu = EscMenu()
-                sprites.add(esc_menu)
         if not change_image_time % 80:
             change_image_time = 0
         sprites.draw(screen)
         sprites.update(check=pygame.key.get_pressed(), flag_change_image=change_image_time)
         pygame.display.flip()
         clock.tick(FPS)
+    return False
 
 
-start_screen()
-start_game()
+def main():
+    start_screen()
+    flag = start_game()
+    if flag:
+        main()
 
+
+if __name__ == '__main__':
+    main()
