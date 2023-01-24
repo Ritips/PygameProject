329fbee7977fb2274b4955f1d3263fa6aedc3e87
StormVortex <undem1390@gmail.com>
2023-01-25 00:04:21 +0300

    Continue gaming after ending level

--- Base.py
+++ Base.py
@@ -4,13 +4,20 @@ from StartScreen import start_screen
 from StartLevel import start_level_game
 
 
-def main():
-    start_screen()
-    level, index = LEVELS.get_level().get_level()
+def main(lobby=True, index=None):
+    if lobby:
+        start_screen()
+        level, index = LEVELS.get_level().get_level()
+    else:
+        if index:
+            LEVELS.chose_level(level_chosen=index)
+            level, index = LEVELS.get_level().get_level()
     if index == 0:
         flag = start_level_game()
         if flag == 110101:
             main()
+        if flag == 110102:
+            main(lobby=False, index=1)
     if index == 1:
         flag = queen_level()
         if flag == 110101:
