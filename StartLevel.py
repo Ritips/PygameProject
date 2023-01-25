89204d2d70c4f836bebe6000a8c0f9f7924aa1b2
StormVortex <undem1390@gmail.com>
2023-01-25 21:54:25 +0300

    Corrections

--- StartLevel.py
+++ StartLevel.py
@@ -150,7 +150,6 @@ def start_level_game():
     for construction in constructions:
         construction.kill()
     class_level = LEVELS.get_level()
-    print(class_level)
     level_to_draw, index = class_level.get_level()
     color, player = draw_level(index=index, level_draw=level_to_draw)
     change_image_time = 0
